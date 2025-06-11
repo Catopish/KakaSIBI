7570271dfb4fd802a7544881bc130962b908325c
Al <athila2001@gmail.com>
2025-06-11 08:31:21 +0700

    first init

---
+++ MachineLearningChallenge/MachineLearningChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MachineLearningChallengeApp.swift
+//  MachineLearningChallenge
+//
+//  Created by Al Amin Dwiesta on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MachineLearningChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
