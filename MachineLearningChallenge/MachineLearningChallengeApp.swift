a210910db7c2cc2ba1927d4ba2bbf769525a8154
ndan1 <lindanchristiano10@gmail.com>
2025-06-14 18:12:30 +0700

    init + word selection card

--- MachineLearningChallenge/MachineLearningChallengeApp.swift
+++ MachineLearningChallenge/MachineLearningChallengeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MachineLearningChallengeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TestingView()
         }
     }
 }
