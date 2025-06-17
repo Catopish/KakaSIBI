c561f90ecf7279bb22fb83d5252c6837e48bf51e
Geokenny23 <kennygun2308@gmail.com>
2025-06-17 10:39:23 +0700

    feat:tambahin road di background

--- Game/GamePreview.swift
+++ Game/GamePreview.swift
@@ -6,23 +6,23 @@ import SpriteKit
 struct GamePreview: View {
     @StateObject private var cameraModel = CameraModel()
 
-    var scene: SKScene {
-        let s = GameScene(size: CGSize(width: 800, height: 600), cameraModel: cameraModel)
-        s.scaleMode = .resizeFill
-        return s
-    }
-
     var body: some View {
-        ZStack {
-            CameraPreview(session: cameraModel.session)
-            TransparentSpriteView(scene: scene)
+        GeometryReader { geometry in
+            ZStack {
+                CameraPreview(session: cameraModel.session)
+
+                TransparentSpriteView(
+                    scene: GameScene(size: geometry.size, cameraModel: cameraModel)
+                )
                 .ignoresSafeArea()
-        }
-        .onAppear {
-            cameraModel.start()
+            }
+            .onAppear {
+                cameraModel.start()
+            }
         }
     }
 }
+
 
 
 #Preview {
